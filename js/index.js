fee9f962fabf2e655960dfe80a2421905b3bfb54
Nii <niioto@Lively.local>
2016-12-15 13:25:30 +0000

    random

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('body').scrollspy({ target: '#navi' });
+});
+
+$(window).load(function(){
+    var $container = $('.gridc');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.grid a').click(function(){
+        $('.grid .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+});
